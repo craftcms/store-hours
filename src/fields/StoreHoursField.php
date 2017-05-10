f84c8eb60d32c20659acf7619d699ae5d7de2f13
Makeila Lundy <makeila@pixelandtonic.com>
2017-05-10 12:44:18 -0700

    Finished Craft 3 port

--- src/fields/StoreHoursField.php
+++ src/fields/StoreHoursField.php
@@ -2,7 +2,8 @@
 namespace craft\storehours\fields;
 
 use craft\base\ElementInterface;
-use craft\fields\PlainText;
+use craft\base\Field;
+use craft\helpers\Json;
 use DateTime;
 use Craft;
 use yii\db\Schema;
@@ -12,7 +13,7 @@ use yii\db\Schema;
  *
  * @property string $contentColumnType
  */
-class StoreHoursField extends PlainText
+class StoreHoursField extends Field
 {
     /**
      * @return string
@@ -46,77 +47,80 @@ class StoreHoursField extends PlainText
      * @param ElementInterface|null $element
      * @return string
      */
-	public function getInputHtml($value, ElementInterface $element = null): string
-	{
-		return Craft::$app->getView()->renderTemplate('storeHours/input', array(
-			'id'    => Craft::$app->view->formatInputId($this->handle),
-			'name'  => $this->handle,
-			'value' => $value,
-		));
-	}
+    public function getInputHtml($value, ElementInterface $element = null): string
+    {
+        $value = Json::decode($value);
 
-	/**
-	 * Returns the input value as it should be saved to the database.
-	 *
-	 * @param mixed $value
-	 * @return mixed
-	 */
-	public function prepValueFromPost($value)
-	{
-		$this->_convertTimes($value, Craft::$app->getTimeZone());
+        return Craft::$app->getView()->renderTemplate('storeHours/input', array(
+            'id'    => Craft::$app->view->formatInputId($this->handle),
+            'name'  => $this->handle,
+            'value' => $value,
+        ));
+    }
 
-		return $value;
-	}
-
-	/**
-	 * Prepares the field's value for use.
-	 *
-	 * @param mixed $value
-	 * @return mixed
-	 */
-	public function prepValue($value)
-	{
-		$this->_convertTimes($value);
-
-		return $value;
-	}
-
-	/**
-	 * Loops through the data and converts the times to DateTime objects.
-	 *
-	 * @access private
-	 *
-	 * @param array &$value
-	 * @param null  $timezone
-	 *
-	 * @return null
-	 */
-	private function _convertTimes(&$value, $timezone = null)
-	{
-		if (is_array($value))
-		{
-			foreach ($value as &$day)
-			{
-				if ((is_string($day['open']) && $day['open']) || (is_array($day['open']) && $day['open']['time']))
-				{
-					$day['open'] = DateTime::createFromFormat($day['open'], $timezone);
-				}
-				else if (!($day['open'] instanceof DateTime))
-				{
-					$day['open'] = '';
-				}
-
-				if ((is_string($day['close']) && $day['close']) || (is_array($day['close']) && $day['close']['time']))
-				{
-					$day['close'] = DateTime::createFromFormat($day['close'], $timezone);
-				}
-				else if (!($day['close'] instanceof DateTime))
-				{
-					$day['close'] = '';
-				}
-			}
-		}
-	}
+    /**
+     * Returns the input value as it should be saved to the database.
+     *
+     * @param mixed $value
+     * @return mixed
+     */
+    public function prepValueFromPost($value)
+    {
+        $this->_convertTimes($value, Craft::$app->getTimeZone());
 
 
+        return $value;
+    }
+
+    /**
+     * Prepares the field's value for use.
+     *
+     * @param mixed $value
+     * @return mixed
+     */
+    public function prepValue($value)
+    {
+        $this->_convertTimes($value);
+
+
+
+        return $value;
+    }
+
+    /**
+     * Loops through the data and converts the times to DateTime objects.
+     *
+     * @access private
+     *
+     * @param array &$value
+     * @param null  $timezone
+     *
+     * @return null
+     */
+    private function _convertTimes(&$value, $timezone = null)
+    {
+        if (is_array($value))
+        {
+            foreach ($value as &$day)
+            {
+                if ((is_string($day['open']) && $day['open']) || (is_array($day['open']) && $day['open']['time']))
+                {
+                    $day['open'] = DateTime::createFromFormat($day['open'], $timezone);
+                }
+                else if (!($day['open'] instanceof DateTime))
+                {
+                    $day['open'] = '';
+                }
+
+                if ((is_string($day['close']) && $day['close']) || (is_array($day['close']) && $day['close']['time']))
+                {
+                    $day['close'] = DateTime::createFromFormat($day['close'], $timezone);
+                }
+                else if (!($day['close'] instanceof DateTime))
+                {
+                    $day['close'] = '';
+                }
+            }
+        }
+    }
 }
